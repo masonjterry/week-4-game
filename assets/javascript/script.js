ac4a159d58bd04584005817f6bbb953a0f2070b8
unknown <masonjterry@gmail.com>
2017-11-15 07:45:09 -0600

    js changes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,13 +17,18 @@ var p3CounterAttackPower = 30;
 var p4CounterAttackPower = 60;
 
 // Results
-var result1;
-var result2;
+var playerResult;
+var opponentResult;
 
 // Buttons
 var playerClick;
 var opponentClick;
 var attackButton;
+
+var player;
+var opponent;
+
+var playerPicked = false;
 
 $(document).ready( function() {
 
@@ -38,51 +43,63 @@ $(document).ready( function() {
 
 		if (playerClick === 'box1') {
 			$('#playerLocation').append($('#box1'));
-			$('#box1').attr('id', 'player1');
+			$('#box1').attr('id', 'player');
 			$('#box2 #box3 #box4').addClass('opponent');
+			$('#box2 #box3 #box4').removeClass('player');
+			playerPicked = true;
 		} 	else if (playerClick === 'box2') {
 			$('#playerLocation').append($('#box2'));
-			$('#box2').attr('id', 'player1');
+			$('#box2').attr('id', 'player');
 			$('#box1 #box2 #box4').addClass('opponent');
+			$('#box1 #box3 #box4').removeClass('player');
+			playerPicked = true;
 		} 	else if (playerClick === 'box3') {
 			$('#playerLocation').append($('#box3'));
-			$('#box3').attr('id', 'player1');
+			$('#box3').attr('id', 'player');
 			$('#box1 #box2 #box4').addClass('opponent');
+			$('#box1 #box2 #box4').removeClass('player');
+			playerPicked = true;
 		}	else if (playerClick === 'box4') {
 			$('#playerLocation').append($('#box4'));
-			$('#box4').attr('id', 'player1');
+			$('#box4').attr('id', 'player');
 			$('#box1 #box2 #box3').addClass('opponent');
+			$('#box1 #box2 #box3').removeClass('player');
+			playerPicked = true;
 		}
 
 	};
 
-	function opponentPick() {
+	if (playerPicked) {
 
-		opponentClick = ($(this).attr('class'));
+		function opponentPick() {
 
-		if (opponentClick === 'box1') {
-			$('#opponentLocation').append($('#box1'));
-			$('#box1').attr('id', 'player2');
-		} 	else if (opponentClick === 'box2') {
-			$('#opponentLocation').append($('#box2'));
-			$('#box2').attr('id', 'player2');
-		} 	else if (opponentClick === 'box3') {
-			$('#opponentLocation').append($('#box3'));
-			$('#box3').attr('id', 'player2');
-		}	else if (opponentClick === 'box4') {
-			$('#opponentLocation').append($('#box4'));
-			$('#box4').attr('id', 'player2');
-		}
+			opponentClick = ($(this).attr('class'));
 
-	};
+			if (opponentClick === 'box1') {
+				$('#opponentLocation').append($('#box1'));
+				$('#box1').attr('id', 'opponent');
+			} 	else if (opponentClick === 'box2') {
+				$('#opponentLocation').append($('#box2'));
+				$('#box2').attr('id', 'opponent');
+			} 	else if (opponentClick === 'box3') {
+				$('#opponentLocation').append($('#box3'));
+				$('#box3').attr('id', 'opponent');
+			}	else if (opponentClick === 'box4') {
+				$('#opponentLocation').append($('#box4'));
+				$('#box4').attr('id', 'opponent');
+			}
+
+		};
+
+	}
 
 	function attack() {
 		
 		buttonClick = ($(this).attr('id'));
 
-		if (buttonClick === 'attack') {
-			$('').
-		}
+		// if (buttonClick === 'attack') {
+		// 	playerResult -= 
+		// }
 
 
 	};
@@ -96,4 +113,8 @@ $(document).ready( function() {
 	function restart() {
 		location.reload();
 	};
+
+	var newP = $('<p>' + p1HealthPoints + '</p>')
+
+	$('#box1').append(newP);
 });
